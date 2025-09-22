92159f75da0c563d6ded2e88dd30787b2ffa643e
userone <userone@email.com>
2025-09-22 20:39:02 +0200

    Refactor Header component layout and add Logo component with styles

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { GatsbyBrowser } from "gatsby";
 import Layout from "./src/components/Layout";
+import "./src/styles/global.css";
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
   element,
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,51 +1,27 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import * as styles from "./Header.module.css";
+import Logo from "./Logo";
 
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.brand}>
-        <Link className={styles.link} to="/">
-          it.assonuovavita
-        </Link>
+      <div className={styles.left}>
+        <Logo className={styles.logo} />
       </div>
+      <Link className={styles.title} to="/">
+        Associazione Nuova Vita
+      </Link>
       <nav className={styles.nav} aria-label="Main Navigation">
-        <Link className={styles.link} activeClassName={styles.active} to="/">
-          Home
-        </Link>
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/chi-siamo"
-        >
-          Chi Siamo
-        </Link>
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/servizi"
-        >
-          Servizi
-        </Link>
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/documenti"
-        >
-          Documenti
-        </Link>
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/diventa-socio"
-        >
-          Diventa Socio
-        </Link>
-        <Link className={styles.cta} to="/donate">
-          Dona ora!
-        </Link>
+        <Link className={styles.link} activeClassName={styles.active} to="/">Home</Link>
+        <Link className={styles.link} activeClassName={styles.active} to="/chi-siamo">Chi Siamo</Link>
+        <Link className={styles.link} activeClassName={styles.active} to="/servizi">Servizi</Link>
+        <Link className={styles.link} activeClassName={styles.active} to="/documenti">Documenti</Link>
+        <Link className={styles.link} activeClassName={styles.active} to="/diventa-socio">Diventa Socio</Link>
       </nav>
+      <div className={styles.right}>
+        <Link className={styles.cta} to="/donate">Dona ora!</Link>
+      </div>
     </header>
   );
 };
